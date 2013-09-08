53748d007516920e95fc74229342a64e390d9487
Inti Montenegro de Wit <intiremko@gmail.com>
2013-09-07 22:55:53 -0400

    Created a rails application and added status updates.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '117f80b50f462e07b0fb5d5a536744b3cf62ed4a24d37a0a48b765210e4f0060b163ffc4c6fd7854de0393de79971796e522cf0804d5017fc9ea94b468c45455'
